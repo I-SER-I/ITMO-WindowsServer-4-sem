f78c45e72da197df4c0e66089472027da2892142
I_SER_I <I_SER_I@hotmail.com>
2021-03-17 18:11:32 +0300

    Add lab 2

---
+++ Lab 2/DhcpConfigurator.ps1
@@ -0,0 +1,6 @@
+﻿Install-WindowsFeature -Name 'DHCP' -IncludeManagementTools
+
+Import-Csv -Path .\Scope.csv | Add-DhcpServerv4Scope
+Import-Csv -Path .\ExclusionRange.csv | Add-DhcpServerv4ExclusionRange
+Import-Csv -Path .\OptionValue.csv | Set-DhcpServerv4OptionValue
+Import-Csv -Path .\Failover.csv | Add-DhcpServerv4Failover -AutoStateTransition 1 -Force
