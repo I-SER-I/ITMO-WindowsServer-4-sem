0a1493cab8540d7effa08605d3b345f944cb5f24
Sergei <I_SER_I@hotmail.com>
2021-04-14 02:24:22 +0300

    feat: add task scheduler

---
+++ Lab 4/TaskScheduler.ps1
@@ -0,0 +1,15 @@
+﻿$name = "Task monitoring"
+$description = "Dumps information about running processes to a csv file every 3 minutes"
+$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-File C:\MonitoringLogCreater.ps1'
+$time = New-TimeSpan -Minutes 3
+$trigger = New-ScheduledTaskTrigger -Once -At (Get-Date).Date -RepetitionInterval $time -RepetitionDuration ([timeSpan]::MaxValue)
+$settings = New-ScheduledTaskSettingsSet -DontStopIfGoingOnBatteries -AllowStartIfOnBatteries
+
+try
+{
+    Register-ScheduledTask -TaskName $name -Description $description -Action $action -Trigger $trigger -Settings $settings
+}
+catch
+{
+    Write-Error "Scheduled task is already exist"
+}
