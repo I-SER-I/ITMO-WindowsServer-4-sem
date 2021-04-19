e1382f3e0a655f2fb6b7679ab45ac4e36381a7e2
Sergei <I_SER_I@hotmail.com>
2021-04-19 22:16:52 +0300

    feat: ready 4lab

---
+++ Lab 4/DiskPlaceholder.ps1
@@ -0,0 +1,10 @@
+﻿$count = 0
+try
+{
+    while ($true)
+    {
+        $count++
+        $objFile = [io.file]::Create("E:\$count.txt")
+        $objFile.SetLength(1mb)
+        $objFile.Close()
+    }}catch{    $objFile.Close()    Write-Error "Disk is full"}
---
+++ Lab 4/Tools.ps1
@@ -0,0 +1,16 @@
+﻿$infoPath = "C:\info.txt"
+
+Get-EventLog System | 
+    Where-Object {$_.EventID -contains "6009"} | 
+    Select -First 10 > $infoPath
+
+Get-HotFix | 
+    Sort InstalledOn -Descending | 
+    Select-Object HotFixID, InstalledOn -First 5 >> $infoPath
+
+Get-WinEvent -ListLog * | 
+    Where-Object {$_.RecordCount -AND $_.LastWriteTime -LE (Get-Date).AddDays(-1)} | 
+    ForEach-Object {Get-WinEvent -LogName $_.LogName} | 
+    Group-Object -Property LevelDisplayName |
+    Where-Object {"Ошибка","Предупреждение" -contains $_.Name} | 
+    Select-Object Name, Count >> $infoPath
